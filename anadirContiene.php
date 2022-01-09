5df20408e050ad270720a076391105a4815d13d5
Brackisfawn6 <sanchezmikel2002@gmail.com>
2022-01-09 17:44:23 +0100

    domingo9

---
+++ anadirContiene.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" media="all" href="CSS/listarpizzas.css" />
+    <title> King Pizza </title>
+    <link rel="shortcut icon" href="Imagenes/logo2.png" />
+    
+</head>
+<body>
+
+    <?php
+
+    include "conexion.php";
+    include "funciones.php";
+
+    anadirContiene($conexion);
+
+    ?> 
+
+</body>
+</html>
+
