93168db6af62b86c37be122ab213b67396eee54f
unknown <miguel.lopezhe@elorrieta-errekamari.com>
2022-01-14 02:16:16 +0100

    PREPARADO PARA SUBIR A MASTER

--- conexion.php
+++ conexion.php
@@ -15,7 +15,7 @@ $conexion = mysqli_connect($servername, $username, $password, $database);
 if (!$conexion) {
     die("Connection failed: " . mysqli_connect_error());
 }else{
-    echo "Connected successfully <br><br>";
+    //echo "Connected successfully <br><br>";
 }
 
 ?>
--- funciones.php
+++ funciones.php
@@ -535,6 +535,9 @@ function borrarPizzas($conexion){
         if($_REQUEST['borrarSelect'] == "0"){
             echo "<fieldset><h2>Error: Seleccione una pizza.<br><br>
             <a href='borrarPizzas.php'>Volver</a></h2></fieldset>";
+        }elseif(!mysqli_query($conexion,$sql)){
+            echo "<fieldset><h2>Error: Esta pizza no se puede borrar. Elimine los ingredientes de la pizza primero. <br> Disculpe las molestias.<br><br>
+              <a href='borrarPizzas.php'>Volver</a></h2></fieldset>";
         }else{
             mysqli_query($conexion,$sql);
             echo "<fieldset><h2>Pizza eliminada correctamente.<br><br>
@@ -608,10 +611,13 @@ function borrarIngredientes($conexion){
           if($_REQUEST['borrarSelect'] == "0"){
               echo "<fieldset><h2>Error: Seleccione un ingrediente.<br><br>
               <a href='borrarIngredientes.php'>Volver</a></h2></fieldset>";
+          }elseif(!mysqli_query($conexion,$sql)){
+            echo "<fieldset><h2>Error: Este ingrediente no se puede borrar. Elimine el ingrediente de todas las pizzas primero. <br> Disculpe las molestias.<br><br>
+              <a href='borrarIngredientes.php'>Volver</a></h2></fieldset>";
           }else{
-              mysqli_query($conexion,$sql);
-              echo "<fieldset><h2>Ingrediente eliminado correctamente.<br><br>
-              <a href='borrarIngredientes.php'>Eliminar otro ingrediente</a> &nbsp &nbsp <a href='index2.php'>Volver</a></h2></fieldset>";
+            mysqli_query($conexion,$sql);
+            echo "<fieldset><h2>Ingrediente eliminado correctamente.<br><br>
+            <a href='borrarIngredientes.php'>Eliminar otro ingrediente</a> &nbsp &nbsp <a href='index2.php'>Volver</a></h2></fieldset>";
           }
           mysqli_close($conexion);  
         }else{
@@ -795,77 +801,5 @@ function listarIngredientes($conexion){
     mysqli_close($conexion);  
 }
 
-function index2(){
-
-    echo "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title> King Pizza </title>
-        <link rel='stylesheet' media='all' href='CSS/index2.css' />
-        <link rel='shortcut icon' href='Imagenes/logo2.png' />
-    </head>
-    <body>
-
-    <div class='logo'>
-    <img src='Imagenes/banner2.png' alt='' width='100%' height='100%'>
-    </div>";
-
-    if(!isset($_SESSION)) 
-    { 
-          session_start(); 
-    } 
-    if (isset($_REQUEST['Cerrar'])){
-      $_SESSION["usuario"] = "";
-    }
-    if($_SESSION["usuario"] != ""){
-          echo "<br><fieldset><h2>Session iniciada con el usuario: " . ucwords($_SESSION["usuario"]) . "</h2>";
-          echo "<h2><form action='index.php' method='post'>
-          <input type='submit' value='Cerrar Sesion' name='Cerrar'/>
-          </form></h2></fieldset>";  
-    }
-     
-    echo"
-    <input type='checkbox' id='mmeennuu'>
-
-    <label class='menu' for='mmeennuu'>
-
-        <div class='barry'>
-            <span class='bar'></span>
-            <span class='bar'></span>
-            <span class='bar'></span>
-            <span class='bar'></span>
-        </div>
-        
-        <ul>
-            <li><a id='pizzas' href='listarClientes.php'>Ver Clientes</a></li>
-            <li><a id='pizzas' href='listarPedidos.php'>Ver Pedidos</a></li>
-            <li><a id='pizzas' href='anadirPizzas.php'>Añadir Pizzas</a></li>
-            <li><a id='pizzas' href='borrarPizzas.php'>Borrar Pizzas</a></li>
-            <li><a id='pizzas' href='anadirIngredientes.php'>Añadir Ingredientes</a></li>
-            <li><a id='pizzas' href='borrarIngredientes.php'>Borrar Ingredientes</a></li>
-            <li><a id='pizzas' href='anadirContiene.php'>Añadir Contiene</a></li>
-            <li><a id='pizzas' href='borrarContiene.php'>Borrar Contiene</a></li>
-            <li><a id='pizzas' href='listarIngredientes.php'>Ver Ingredientes</a></li>
-            <li><a id='menus' href='listarPizzas2.php'>Ver La Carta</a></li><br>
-            <li><a id='inicioSesion' href='login.php'>Login</a></li>
-        </ul>
-
-    </label>
-
-
-    <br><br><img src='Imagenes/ibai3.png' alt='' width='70%'' height='70%''> <br><br><br><br><br><br><br><br>
-
-    <div class='footer'>
-        <p>Creadores: Miguel Lopez, Mikel Sanchez, Felix Miguel. </p>
-    </div>
-
-
-    </body>
-    </html>";
-    
-}
 
 ?>
